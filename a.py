3fb85afc0f0198c7a370d6bbd01396554775e74b
nwweiit <nwweiit@gmail.com>
2025-10-30 14:56:18 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
-for a in range(1,101) :
+for i in range(1,101) :
     if i % 2 == 0 :
         print(a, "2의 배수입니다.")
     if i % 5 == 0 :
-        print(a, "5의 배수입니다.")
+        print(i, "5의 배수입니다.")
     else :
-        print(a)
+        print(i)
